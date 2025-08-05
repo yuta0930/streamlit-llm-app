c93d735bd55b2f87d0e9e2b2921cb6d6ee608c3c
yuta <daikiyuta711668@gmail.com>
2025-08-05 19:24:12 +0900

    first commit.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
+import os
 api_key = os.getenv("OPENAI_API_KEY")
 if api_key is None:
     raise ValueError("OPENAI_API_KEY is not set in the environment.")
